ee45b8634e3e4f5f650573b6506a482a26a5927f
Anastasiia Duravkina <duravkinaanastasiia@gmail.com>
2026-03-22 19:25:32 -0400

    Update connect4_logic.py

--- connect4_logic.py
+++ connect4_logic.py
@@ -3,36 +3,50 @@
 
 import numpy as np
 
+#size of the board
+column_count = 7
+row_count = 6
+
 # creating the board for the game 
 def create_board():
-  board = np.zeros((6,7))
+  board = np.zeros((row_count,column_count))
   return board
 
 board = create_board()
 
-# the game will begin with the game_over being false 
+# The game will begin with the game_over being false 
 game_over = False
 
-#to diferentiate whose turn it is
+# Differentiating whose turn it is 
 turn = 1
 
-def is_valid(column):
+# Function that checks if the move is valid (is there a column to drop it in)
+def is_valid_existing_column(column):
    if column <= 7 :
       return True
    else:
       return False
-
+   
+def is_valid_filled__column(column):
+  for row in board:
+      if row[column] == 0:
+         return True
+   
+  return False
+   
+   
+# Function that moves the piece
 def moving_pieces(colums, player) :
-   for row in range(board.shape[0] - 1, -1, -1):   # go through each row
+   for row in range(row_count - 1, -1, -1):   # go through each row
       if board[row][colums] == 0:     # check if the value is 0
           board[row][colums] = player      # replace it with 1
           break 
       
-
+# Fuction that checks if the game has been won
 def check_win(player):
    game_over = False
-   for row in range(board.shape[0]):          # go through each row
-      for col in range(board.shape[1] - 3):  # check groups of 4
+   for row in range(row_count):          # go through each row
+      for col in range(column_count - 3):  # check groups of 4
         if (board[row][col] == player and
             board[row][col+1] == player and
             board[row][col+2] == player and
@@ -40,8 +54,8 @@ def check_win(player):
               game_over = True
 
 
-   for col in range(board.shape[1]):          # go through each column
-      for row in range(board.shape[0] - 3):  # check groups of 4 vertically
+   for col in range(column_count):          # go through each column
+      for row in range(row_count - 3):  # check groups of 4 vertically
         if (board[row][col] == player and
             board[row+1][col] == player and
             board[row+2][col] == player and
@@ -50,16 +64,16 @@ def check_win(player):
 
   
   # Check the positevely Sloped diagonals 
-   for col in range(board.shape[1]-3):
-      for row in range(board.shape[0]-3):
+   for col in range(column_count-3):
+      for row in range(row_count-3):
         if( board[row][col]==player and
             board[row+1][col+1]==player and
             board[row+2][col+2]==player and
             board[row+3][col+3]==player ):
               game_over = True
 
-   for col in range(board.shape[1] - 3):
-        for row in range(3, board.shape[0]):
+   for col in range(column_count - 3):
+        for row in range(3, row_count):
            if( board[row][col]==player and
             board[row-1][col+1]==player and
             board[row-2][col+2]==player and
@@ -71,18 +85,26 @@ def check_win(player):
    else:
       return False
 
+# ============================
+#      Game Dynamics 
+# ============================
 
-
-
-
-# dynamics of the game 
 while game_over == False:
-  # ask for player 1 input
-  if turn == 1:
-    selected_column = int(input("Player 1, choose a column (0-6): ")) - 1 # takes in the answer from the player and converts it to a number
 
-    is_valid(selected_column)
-   
+# ====================
+#      Player 1
+# ====================
+
+  if turn == 1:
+
+    # takes in the answer from the player and converts it to a number
+    selected_column = int(input("Player 1, choose a column (0-6): ")) - 1 
+
+    is_valid_existing_column(selected_column)
+    print(is_valid_filled__column(selected_column))
+
+
+   # Placing the piece on the board
     moving_pieces(selected_column, 1)
 
     for row in board:
@@ -90,21 +112,25 @@ while game_over == False:
     
     turn = 2
 
-    
     game_over = check_win(1) 
     
     if game_over:
         print("Player 1 wins!")
         break
     
-# ask for player 2 input 
+# ====================
+#      Player 2
+# ====================
+
   if turn == 2:
+
+    # takes in the answer from the player and converts it to a number
     selected_column_second = int(input("Player 2, choose a column (0-6): ")) - 1
 
-   # is it valid
-    is_valid(selected_column_second)
+    is_valid_filled__column(selected_column_second)
+    is_valid_existing_column(selected_column_second)
     
-    # move the piece 
+    # Placing the piece on the board
     moving_pieces(selected_column_second, 2)
 
     for row in board:
